5f0132e7d6ccfdcf97901fa5b58cf4464e38c276
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-01-06 23:36:14 -0500

    Set up Backend of Server

---
+++ server/controllers/person.controller.js
@@ -0,0 +1,5 @@
+module.exports.index = (req, res) => { //req = request, res = response
+    res.json({
+        message: "Hello World"
+    });
+}
--- server/server.js
+++ server/server.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+require('./routes/person.routes')(app);
+
 app.listen(port, () => console.log(`listening on port: ${port}`));
