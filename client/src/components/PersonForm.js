2d2c4dea26dc3bca9bc9718efd08d7043f8dc9dd
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-01-07 21:29:01 -0500

    First Complete Front end and Backend with React

--- client/src/components/PersonForm.js
+++ client/src/components/PersonForm.js
@@ -1,2 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from axios;
+
+const PersonForm= () => {
+    const [ message, setMessage ] = useState("Message loading in your mom");
+    useEffect(() => {
+        axios.get("http://localhost:8000/api")
+            .then(res=>setMessage(res.data.message))
+            .catch(err=>console.log(err))
+    }, []);
+    retrun (
+        <div>
+            <h1>The message from your mom: {message}</h1>
+        </div>
+    )
+}
+export default PersonForm;
