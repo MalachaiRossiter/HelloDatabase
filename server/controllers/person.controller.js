8e817895f9c6d0aad1e7c38c248cc48e07b5ddbc
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-01-08 23:30:57 -0500

    creating objects in a database using express

--- server/controllers/person.controller.js
+++ server/controllers/person.controller.js
@@ -1,5 +1,13 @@
+const Person = require('../models/person.model');
+
 module.exports.index = (req, res) => { //req = request, res = response
     res.json({
         message: "Hello World"
     });
+}
+
+module.exports.createPerson = (req, res) => {
+    Person.create(req.body)
+        .then(person => res.json(person))
+        .catch(err => res.json(err));
 }
--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors())
-const port = 8000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+require('./config/mongoose.config');
 require('./routes/person.routes')(app);
 
-app.listen(port, () => console.log(`listening on port: ${port}`));
+app.listen(8000, () => console.log(`listening on port: 8000`));
